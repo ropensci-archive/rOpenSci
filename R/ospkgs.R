9d8d940691808dabc814ddf0a342e5b7044bb66e
Scott Chamberlain <myrmecocystus@gmail.com>
2011-09-05 09:55:57 -0500

    added function ospkgs to find results across package depends for species names

---
+++ R/ospkgs.R
@@ -0,0 +1,27 @@
+# ospkgs.R
+
+ospkgs <- function(species = NA)
+# Args:
+#   species: species name (character)
+# Examples:
+#   ospkgs(species = "Delphinus")
+{
+  treeBASE_out <- suppressMessages(
+    search_treebase(paste('"', species, '"', sep = ""), by="taxon"))
+  rplos_out <- searchplos(species, 'everything', 999)
+  rgbif_out <- taxoncount(species)
+  rbhl_out <- namesearch(species = "poa annua", format = "json")
+  outlist <- list(treeBASE = c(length(treeBASE_out), class(treeBASE_out)))
+  outlist$rplos <- c(as.numeric(rplos_out[[1]]), "papers")
+  outlist$rgbif <- c(rgbif_out, "records")
+  outlist$rbhl <- c(length(rbhl_out[,2]), "records")
+  df <- ldply(outlist)
+  names(df) <- c("Package", "Number", "Datatype")
+  return(df)
+}
+
+
+
+
+# suppressMessages(search_treebase(paste('"', species, '"', sep = ""), by="taxon"))
+
