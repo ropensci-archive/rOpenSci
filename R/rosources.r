565118f5fcf5b41047315d4f38989ba4b8e8a10d
Karthik Ram <karthik.ram@gmail.com>
2012-01-26 14:39:22 -0800

    Function to list current sources

---
+++ R/rosources.r
@@ -0,0 +1,13 @@
+
+#'
+#' List all data sources currently served through ropensci
+#' @param  none
+#' @examples \dontrun{
+#' rosources()
+#' }
+rosources <- function(sources='data/rosources.rda')
+{
+load(sources)	
+return(rosources)	
+}
+
