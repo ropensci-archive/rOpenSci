86a69904afce6043a6a31ce6e3b83be819c13590
Karthik Ram <karthik.ram@gmail.com>
2012-03-12 16:14:19 -0700

    Renamed file.

--- R/ro_sources.r
+++ R/ro_sources.r
@@ -1,11 +1,10 @@
-
 #'
 #' List all data sources currently served through ropensci
 #' @param  none
 #' @examples \dontrun{
 #' rosources()
 #' }
-rosources <- function(sources='data/rosources.rda')
+ros_sources <- function(sources='data/rosources.rda')
 {
 load(sources)	
 return(rosources)	
