0bfb12e348b87a0d061fe5639ad6f7c24dedf78a
Karthik Ram <karthik.ram@gmail.com>
2012-03-21 12:10:05 -0700

    Template for function is now in place.

--- R/ro_search.r
+++ R/ro_search.r
@@ -1,7 +1,23 @@
-
+#'A simple search across existing packages
+#'
+#'<longer description>
+#'@param query = NULL <what param does>
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
 ro_search <- function(query = NULL) {
-	if(!is.null(query)) 
-	{
-		# search for terms in all packages one by one.
-	}
+if(!is.null(query)) {
+	stop("You did not include a query term", call.= FALSE)
+ }
+# source_list=ro_sources() 
+# # load packages
+# search rPLOS
+# search RMendeley
+# search rDryad (tricky because there is no API based search method)
+# concat the results and return it.
 }
