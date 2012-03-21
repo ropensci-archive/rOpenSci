b584e49d342f48dd7d15085810a644803763480f
Karthik Ram <karthik.ram@gmail.com>
2012-03-21 12:06:17 -0700

    Set up a basic list of ropensci packages, mostly ones on CRAN.

--- R/ro_sources.r
+++ R/ro_sources.r
@@ -1,12 +1,28 @@
+
+
+#'Package to list current ropensci sources 
 #'
-#' List all data sources currently served through ropensci
-#' @param  none
-#' @examples \dontrun{
-#' rosources()
-#' }
-ros_sources <- function(sources='data/rosources.rda')
-{
+#'@param sources Reads data stored in data folder
+#'@param  verbose = TRUE Default is complete listing
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+ros_sources <- function(sources='data/rosources.rda', verbose = TRUE, working = NULL) {
 load(sources)	
+if(!is.null(working)) {
+	sources = subset(sources, working == TRUE)
+}
 return(rosources)	
 }
 
+# Generating package data
+# data_source = c("Dryad", "Mendeley", "PLOS")
+# package_name = c("rDryad", "RMendeley", "rPLOS")
+# working = c(TRUE, TRUE, TRUE)
+# sources=data.frame(data_source, package_name, working)
+# save(sources, file="../data/rosources.rda")
